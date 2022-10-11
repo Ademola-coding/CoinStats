0947c44cd787ce8934de6e353ccfa092617e528e
ademola <ademolaowoeye07@gmail.com>
2022-10-11 19:55:42 +0100

    Created header and basic setup

---
+++ src/App.js
@@ -0,0 +1,16 @@
+ /* eslint-disable */
+import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
+import NavBar from './component/NavBar/NavBar';
+
+function App() {
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <NavBar />
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/component/NavBar/NavBar.js
@@ -0,0 +1,17 @@
+ /* eslint-disable */
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './navBar.css';
+
+function NavBar() {
+  return (
+    <div className="NavBar">
+      <Link className="navLink" to="/"><h1 className="nameNav">My Wallet</h1></Link>
+      <div className="coinNav">
+        <p className="settingNav">⚙️</p>
+      </div>
+    </div>
+  );
+}
+
+export default NavBar;
